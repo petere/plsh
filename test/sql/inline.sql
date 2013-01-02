0d6570ff79f0a286d69c9b4f1fb49884af4c9987
Peter Eisentraut <peter@eisentraut.org>
2013-01-01 22:39:13 -0500

    Fix test suite for Travis CI
    Files created in /tmp must apparently be made readable explicitly.

--- test/sql/inline.sql
+++ test/sql/inline.sql
@@ -1,6 +1,6 @@
 \! mkdir /tmp/plsh-test && chmod a+rwx /tmp/plsh-test
 
-DO E'#!/bin/sh\necho inline > /tmp/plsh-test/inline.out' LANGUAGE plsh;
+DO E'#!/bin/sh\necho inline > /tmp/plsh-test/inline.out; chmod a+r /tmp/plsh-test/inline.out' LANGUAGE plsh;
 
 \! cat /tmp/plsh-test/inline.out
 \! rm -r /tmp/plsh-test
