72c50de25674435101ef728bd1b634bd9b61b81f
Peter Eisentraut <peter@eisentraut.org>
2013-02-01 22:07:58 -0500

    Don't install into pg_catalog
    The extension is now really relocatable.  This was broken before.

--- plsh--1--2.sql
+++ plsh--1--2.sql
@@ -1,8 +1,8 @@
-CREATE FUNCTION pg_catalog.plsh_inline_handler(internal) RETURNS void
+CREATE FUNCTION plsh_inline_handler(internal) RETURNS void
     AS '$libdir/plsh'
     LANGUAGE C;
 
 CREATE OR REPLACE LANGUAGE plsh
-    HANDLER pg_catalog.plsh_handler
-    INLINE pg_catalog.plsh_inline_handler
-    VALIDATOR pg_catalog.plsh_validator;
+    HANDLER plsh_handler
+    INLINE plsh_inline_handler
+    VALIDATOR plsh_validator;
